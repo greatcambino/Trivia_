82ac4520399e5d5bc3ef88e34eb971547f5231d0
TERRENCE CAMPBELL <TLCAMPBELL89@GMAIL.COM>
2017-12-12 11:54:46 -0500

    retooled html css

--- trivia.js
+++ trivia.js
@@ -1,1 +1,20 @@
-// Tit
+class question {
+    constructor(question, choice1, choice2, choice3, correctChoice) {
+        this.question = ""
+        this.choice1 = ""
+        this.choice2 = ""
+        this.choice3 = ""
+        this.correctChoice = ""
+    }
+
+    checkAnswer(userChoice){
+    }
+}
+
+const question1 = new question('"JK" Rowling?', "Jennifer Kristin", "Julia Kate", "Jane Kennedy", "Joanne K.")
+const question2 = new question('"RL" Stine?', "Richard Langdon", "Robert London", "Ricky LeFleur", "Robert Lawrence")
+const question3 = new question('"CS" Lewis?', "Corey Smitka", "Charles Sweeney", "Christopher Stine", "Clive Staples")
+
+
+const 
+\\\\\\
